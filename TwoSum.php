427cc484be69348caf1977c1df919b9d2458a728
d_e_n_n_y <dengzy@c-bim.net>
2019-07-11 22:49:43 +0800

    en: add the first algorithm /level easy

---
+++ TwoSum.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Denny
+ * Date: 7/11/2019
+ * Time: 10:27 PM
+ */
+
+/**
+ * @param Integer[] $nums
+ * @param Integer $target
+ * @return Integer[]
+ */
+function twoSum($nums, $target) {
+    //if(!$target) return null;
+    $len = count($nums);
+    for($i=0; $i<$len; $i++){
+        $r = $target - $nums[$i];
+        for($j=$i+1; $j<$len; $j++){
+            if($r == $nums[$j]){
+                return [$i, $j];
+            }
+        }
+    }
+    return null;
+}
+
+$nums = [1, 3, 2, 7, 11, 15];
+$target = 9;
+
+var_dump(twoSum($nums, $target));
